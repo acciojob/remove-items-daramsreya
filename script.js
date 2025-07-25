873666cac0d33d5060249cc8ecb4fec3653e0b0e
daramsreya83@gmail.com <your.email@example.com>
2025-07-25 23:05:12 +0530

    Assigment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const colorSelector=document.querySelector("#colorSelector");
+const colorSelector=document.querySelector("#colorSelect");
 const button=document.querySelector("input[type=button]");
 button.addEventListener('click',function(e){
     const selected=colorSelector.selectedIndex;
