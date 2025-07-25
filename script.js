e95f2fd7cba7137175797f8dfb53310c3e6f80dd
daramsreya83@gmail.com <your.email@example.com>
2025-07-25 22:56:19 +0530

    remove

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const colorSelector=document.querySelector("#colorSelector");
+const button=document.querySelector("input[type=button]");
+button.addEventListener('click',function(e){
+    const selected=colorSelector.selectedIndex;
+    colorSelector.remove(selected);
+}) 
